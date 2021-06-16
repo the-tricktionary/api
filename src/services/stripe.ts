8b35f26455a8749d1b7c683509e8ca48027a87d5
Svante Bengtson <svante@swantzter.se>
2021-06-16 23:59:24 +0000

    fix typo

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,5 +1,6 @@
 import Stripe from 'stripe'
 import { STRIPE_SK } from '../config'
+import { logger } from './logger'
 
 const stripe = new Stripe(STRIPE_SK as string, { apiVersion: '2020-08-27' })
 
@@ -8,7 +9,9 @@ const stripe = new Stripe(STRIPE_SK as string, { apiVersion: '2020-08-27' })
 export async function getProducts () {
   const products = await stripe.products.list()
 
-  return products.data.filter(p => p.active && p.metadata.shop === 'the-tricktionary')
+  logger.debug(products.data)
+
+  return products.data.filter(p => p.active && p.metadata.store === 'the-tricktionary')
 }
 
 export async function getPrices (productId: string) {
