3212fdef175caff20a4995c9ff7461a0351d4970
Svante Bengtson <svante@swantzter.se>
2026-09-18 22:41:58 +0200

    cache users much shorter for permissions to propagate

--- src/services/authentication.ts
+++ src/services/authentication.ts
@@ -32,7 +32,7 @@ export async function userFromAuthorizationHeader (header: string | undefined, {
   }
 
   logger.debug({ uid: decoded.uid }, 'Finding user')
-  let user = await dataSources.users.findOneById(decoded.uid, { ttl: 3600 })
+  let user = await dataSources.users.findOneById(decoded.uid, { ttl: 30 })
 
   if (!user) {
     user = await dataSources.users.createOne({
